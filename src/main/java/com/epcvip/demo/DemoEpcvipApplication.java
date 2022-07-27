62b79109ac16187837bb00470668b3e03c6d22d4
sunilkumarbs <sunilkumarbs05@gmail.com>
2022-07-27 18:10:20 +0530

    project skeleton

---
+++ src/main/java/com/epcvip/demo/DemoEpcvipApplication.java
@@ -0,0 +1,13 @@
+package com.epcvip.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEpcvipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoEpcvipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epcvip/demo/DemoEpcvipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epcvip.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoEpcvipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
